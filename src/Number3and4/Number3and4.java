376b51c488d4c394cc4088f08515b717f64932e8
QuintinKerns <qakern8560@ung.edu>
2019-02-05 19:33:13 -0500

    Numbers 3 and 4 Done

--- src/Number3and4/Number3and4.java
+++ src/Number3and4/Number3and4.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Number3and4 {
-	public static List<Integer> makeList( int N ) // 4N + 4
+	public static List<Integer> makeList( int N ) // 5N + 4
 	{
 		ArrayList<Integer> lst = new ArrayList<Integer>( ); // 2: 1 for new and 1 for assignment
-		for( int i = 0; i < N; i++ ) { // 4N + 1: 4N from the conditional, i++, add(), and trimToSize().
+		for( int i = 0; i < N; i++ ) { // 4N + 1: 5N from the conditional, i++ [i = i + 1], add(), and trimToSize().
 									   // The +1 from assignment of 0 to i
 			lst.add( i ); // 1
 			lst.trimToSize( ); // 1
@@ -15,10 +15,10 @@ public class Number3and4 {
 		return lst; // 1
 	}
 
-	public static void removeFirstHalf( List<?> lst ) // 3N + 4
+	public static void removeFirstHalf( List<?> lst ) // 4N + 4
 	{
 		int theSize = lst.size( ) / 2; // 3. 1 For Assignment, 1 for size(), and one for division
-		for( int i = 0; i < theSize; i++ ) // 3N + 1: 3N from conditional, i++, and remove().
+		for( int i = 0; i < theSize; i++ ) // 3N + 1: 4N from conditional, i++ [i = i + 1], and remove().
 										   //+1 from assignment of 0 to i
 			lst.remove( 0 ); // 1
 	}
